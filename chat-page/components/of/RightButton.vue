c706ca47bded97c8a754c42bc1c60f16e92d4722
你浩 <1047583808@qq.com>
2024-09-20 17:50:15 +0800

    加油

---
+++ chat-page/components/of/RightButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="right-cz pd-6" :style="'left:' + left + 'px;top:' + top + 'px'">
+    <div>{{ left }} --- {{ top }}</div>
+    <div>{{ left }} --- {{ top }}</div>
+    <div>{{ left }} --- {{ top }}</div>
+    <div>{{ left }} --- {{ top }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['event']);
+const props = defineProps({
+  left: {
+    default: 0,
+    type: Number
+  },
+  top: {
+    default: 0,
+    type: Number
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.right-cz {
+  position: fixed;
+  width: 110px;
+  height: 150px;
+  border-radius: $px-4;
+  box-shadow: 0 0 $px-2 $px-1 $ft-color-tips-2;
+  background-color: #8e5bb4;
+}
+</style>
