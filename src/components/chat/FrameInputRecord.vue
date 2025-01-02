e1883e524137d52f81260e1c85970f665b4f9647
你浩 <1047583808@qq.com>
2025-01-02 16:16:47 +0800

    聊天记录布局

--- src/components/chat/FrameInputRecord.vue
+++ src/components/chat/FrameInputRecord.vue
@@ -1,6 +1,6 @@
 <template>
   <n-modal v-model:show='show' :on-after-leave='closeModal'>
-    <n-card style='width: 600px; height: 480px;' class='border-10' :bordered='false' size='huge' role='dialog'
+    <n-card style='width: 600px; height: 550px;' class='border-10' :bordered='false' size='huge' role='dialog'
             aria-modal='true'>
       <div class='chat-history'>
         <span class='flex-center-center'>{{ user.name }}</span>
@@ -8,11 +8,11 @@
         <div class='chat-history-header pd-16'>
           <n-input-group>
             <n-input v-model:value='record.search' placeholder='搜索聊天内容' clearable />
-            <n-button type='primary' ghost @click='searchHistory'>搜 索</n-button>
+            <n-button type='primary' ghost @click='searchHistory()'>搜 索</n-button>
           </n-input-group>
 
           <div class='mt-12 flex-center-zy'>
-            <n-radio-group v-model:value='record.type' size='small' class='mr-16'>
+            <n-radio-group v-model:value='record.type' size='small' class='mr-16' :on-update:value='upType'>
               <n-radio-button :value='0'>全部</n-radio-button>
               <n-radio-button :value='1'>文本</n-radio-button>
               <n-radio-button :value='2'>图片</n-radio-button>
@@ -31,26 +31,20 @@
               <n-empty description='暂无聊天记录' />
             </div>
             <div v-else class='flex-down'>
-              <div v-for='(item, index) in historyData'
-                   :key='index'
-                   class='flex-down mb-16'
-                   :class="item.sendUid === user.uid ? 'align-self-end' : 'align-self-start'"
-                   style='max-width: 80%'>
-                <div class='ft-13' style='color: rgba(0, 0, 0, 0.45); margin-bottom: 4px'
-                     :class="item.sendUid === user.uid ? 'text-right' : ''">
-                  {{ getTimeFormat(item.date) }}
-                </div>
-                <div class='history-item-content'
-                     :style='`background-color: ${item.sendUid === user.uid ? param.style.rightChatBgColor : param.style.leftChatBgColor}`'>
-                  <template v-if='item.type === 0'>
-                    {{ item.message }}
-                  </template>
-                  <n-image
-                    v-else-if='item.type === 1'
-                    :src='item.message'
-                    :preview-src='item.message'
-                    class='history-image'
-                  />
+              <div class='history-message pd-12 flex' v-for='i in 20'>
+                <div
+                  class='user-head flex-center-center'
+                  :style="'background-color:' + tranColor(param.userInfo.photo)"
+                  v-html='computePhoto(param.userInfo.photo)'></div>
+                <div class='flex-down ml-18 w-auto'>
+                  <div class='flex-center-zy'>
+                    <div class='ft-16'>昵称</div>
+                    <div class='ft-color-tips'>时间</div>
+                  </div>
+                  <div>
+                    <span class='history-message-span'>数数据数据数据据</span>
+                    <n-image class='chat-image' src='' />
+                  </div>
                 </div>
               </div>
             </div>
@@ -64,13 +58,15 @@
 <script setup lang='ts'>
 import { createDiscreteApi } from 'naive-ui';
 import { getTimeFormat } from '@/utils/TimeUtil';
+import { tranColor, computePhoto } from '@/utils/OtherUtils';
+import { throttle } from '@/utils/domUtils';
+
+const param = inject<Ref<chatProps>>('param') as chatProps | any;
 
 const { notification } = createDiscreteApi(['notification']);
 
 import { NConfigProvider, zhCN, dateZhCN } from 'naive-ui';
 
-const param = inject<Ref<chatProps>>('param') as chatProps | any;
-const computedStyle = inject<Ref<any>>('computedStyle') as any;
 
 const props = defineProps({
   user: {
@@ -87,13 +83,60 @@ const closeModal = () => {
   emit('closeChatHistory', false);
 };
 
-// 搜索历史记录
-const historyData = ref<any[]>([]);
-const searchHistory = () => {
-  console.log(record, 'record');
+// 上拉拉取消息
+const loadingMessage = ref(false);
+const scrollToTop = () => {
+  let scrollDom = document.getElementsByClassName('chat-history-box')[0];
+  if (scrollDom.scrollTop < 30 && next.value && !loadingMessage.value) {
+    searchHistory(false);
+  }
 };
+
+// 监听滚动条
+const listenerScrollToTop = (state: boolean) => {
+  let scrollDom = document.getElementsByClassName('chat-history-box')[0];
+  if (!scrollDom) return;
+  const throttledScrollToTop = throttle(scrollToTop, 50);
+  if (state) scrollDom.addEventListener('scroll', throttledScrollToTop);
+  else scrollDom.removeEventListener('scroll', throttledScrollToTop);
+};
+
+// 滚动条居底
+const scrollToBottom = () => {
+  setTimeout(() => {
+    let scrollDom = document.getElementsByClassName('chat-history-box')[0];
+    scrollDom.scrollTop = scrollDom.scrollHeight;
+  }, 100);
+};
+
+// 搜索历史记录
+const next = ref(false);
+const historyData = ref<any[]>([{}, {}]);
+const page = ref(1);
+/**
+ * 搜索历史记录
+ * @param isOnePage 是否从第一页开始
+ */
+const searchHistory = (isOnePage: boolean = true) => {
+  if (isOnePage) page.value = 1;
+  else page.value++;
+
+  scrollToBottom();
+  setTimeout(() => {
+    listenerScrollToTop(true);
+  }, 150);
+};
+
+// 日期查询
 const upDate = (val: any) => {
   record.date = val;
+  searchHistory();
+};
+
+// 类型查询
+const upType = (val: any) => {
+  record.type = val;
+  searchHistory();
 };
 
 // 筛选
@@ -110,16 +153,40 @@ const initData = () => {
 };
 
 const show = ref(false);
-watch(() => props.showChatHistory, (newVal) => {
-  if (!newVal) return;
+watchEffect(() => {
+  if (!props.showChatHistory) return;
   initData();
   show.value = true;
   searchHistory();
 });
 
+onBeforeUnmount(() => {
+  listenerScrollToTop(false);
+});
 </script>
 
 <style lang='scss' scoped>
+
+:deep(.chat-image img) {
+  max-width: $px-160;
+  max-height: $px-160;
+}
+
+.history-message {
+  border-bottom: 1px solid rgba(0, 0, 0, 0.06);
+
+  &:last-child {
+    border-bottom: none;
+  }
+
+  &-span {
+    word-wrap: break-word;
+    word-break: break-all;
+    overflow-wrap: anywhere;
+    white-space: pre-wrap;
+  }
+}
+
 .chat-history {
   height: 100%;
   display: flex;
@@ -136,7 +203,7 @@ watch(() => props.showChatHistory, (newVal) => {
   }
 
   &-box {
-    height: $px-320;
+    height: $px-400;
   }
 }
 
